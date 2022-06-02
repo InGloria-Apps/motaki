d5e08813ff5a792c67ceb79073852e86fd3a8da8
Antonio Goulao <antoniomgoulao@gmail.com>
2022-06-03 00:23:06 +0100

    Initial version

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView 
+        style={{ flex: 1 }}
+        injectedJavaScript={`
+         const iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+         if (!iOS) {
+           const meta = document.createElement('meta');
+           let initialScale = 1;
+           if(screen.width <= 800) {
+            initialScale = ((screen.width / window.innerWidth) + 0.1).toFixed(2);
+           }
+           const content = 'width=device-width, initial-scale=' + initialScale ;
+           meta.setAttribute('name', 'viewport');
+           meta.setAttribute('content', content);
+           document.getElementsByTagName('head')[0].appendChild(meta);
+         }
+       `}
+        scalesPageToFit={Platform.OS === 'ios'}
+        originWhitelist={['*']}
+        source={{
+          uri: 'https://arcg.is/1qX4rr'
+        }} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
